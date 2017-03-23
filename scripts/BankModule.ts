c05eea032c54c88313567fa066383be99c6cccca
Mattia Franchetto <mt.franchetto@gmail.com>
2017-03-23 14:30:42 +0100

    Add a bunch of stuff

--- scripts/BankModule.ts
+++ scripts/BankModule.ts
@@ -1,11 +1,26 @@
+import {IModule, IProjectionRegistry, IServiceLocator, IEndpointConfig, ISocketConfig} from "prettygoat";
 import {interfaces} from "inversify";
+import AccountsProjection from "./projections/AccountsProjection";
+import AccountsListProjection from "./projections/AccountsListProjection";
 
-class BankModule {
+class BankModule implements IModule {
 
     modules = (container: interfaces.Container) => {
-
+        container.bind<IEndpointConfig>("IEndpointConfig").toConstantValue({
+            port: 3000
+        });
+        container.bind<ISocketConfig>("ISocketConfig").toConstantValue({
+            path: "/notifications"
+        });
     };
+
+    register(registry: IProjectionRegistry, serviceLocator?: IServiceLocator, overrides?: any): void {
+        registry
+            .add(AccountsListProjection)
+            .add(AccountsProjection, parameters => parameters.id)
+            .forArea("Users");
+    }
 }
 
 
-export {BankModule};
+export default BankModule
--- scripts/projections/AccountsProjection.ts
+++ scripts/projections/AccountsProjection.ts
@@ -1,15 +1,15 @@
 import {IProjectionDefinition, CountSnapshotStrategy, Projection, IProjection} from "prettygoat";
-import {AccountCreated, AccountDeleted, Accounts} from "./TypeDefinitions";
+import {AccountCreated, AccountDeleted, Account} from "../TypeDefinitions";
 
 @Projection("Accounts")
-export class AccountsProjection implements IProjectionDefinition<Accounts> {
+export default class AccountsProjection implements IProjectionDefinition<Account> {
 
-    define(): IProjection<Accounts> {
+    define(): IProjection<Account> {
         let snapshotStrategy = new CountSnapshotStrategy();
         return {
             name: "Accounts",
             definition: {
-                "io.bank.AccountCreated": (state, event: AccountCreated) => {
+                "io.bank.AccountCreated": (state: Account, event: AccountCreated) => {
                     state = {
                         id: event.id,
                         name: event.name,
@@ -17,7 +17,7 @@ export class AccountsProjection implements IProjectionDefinition<Accounts> {
                     };
                     return state;
                 },
-                "io.bank.AccountDeleted": (state, event: AccountDeleted) => {
+                "io.bank.AccountDeleted": (state: Account, event: AccountDeleted) => {
                     state.status = "DELETED";
                     return state;
                 }
